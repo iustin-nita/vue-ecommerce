6262f6d2b1795639e496c2e6c030165c876c378c
Iustin Nita <iustin.nitza@gmail.com>
2018-04-03 20:59:17 +0200

    Add basic routes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import VeeValidate from 'vee-validate';
+
 
 Vue.config.productionTip = false;
+Vue.use(VeeValidate);
 
 /* eslint-disable no-new */
 new Vue({
