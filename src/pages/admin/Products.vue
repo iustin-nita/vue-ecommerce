fd47f59ceffad2afcffe586fa8554a1d74c5ceb4
iustin <iustin.nitza@gmail.com>
2018-04-04 14:09:03 +0200

    Add admin products table

--- src/pages/admin/Products.vue
+++ src/pages/admin/Products.vue
@@ -1,20 +1,44 @@
 <template>
-  <product-list :products="products"></product-list>
+  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+    <table class="table table-striped">
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Manufacturer</th>
+        <th></th>
+        <th></th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="product in products">
+        <td>{{product.name}}</td>
+        <td>${{product.price}}</td>
+        <td>{{product.manufacturer.name}}</td>
+        <td><router-link :to="'/admin/edit/'+product._id"><i class="fa fa-pencil-square-o"></i></router-link></td>
+        <td><a @click="deleteProduct(product._id)" ><i class="fa fa-trash"></i></a></td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-  import ProductList from '../../components/products/ProductList.vue'
   export default {
-    created () {
-      this.$store.dispatch('allProducts')
-    },
     computed: {
       products () {
-        return this.$store.getters.allProducts;
+        return this.$store.getters.allProducts
       }
     },
-    components: {
-      'product-list': ProductList
+    created () {
+      if (this.products.length === 0) {
+        this.$store.dispatch('allProducts')
+      }
+    },
+    methods: {
+      deleteProduct (id) {
+        this.$store.dispatch('removeProduct', id)
+      }
     }
   }
 </script>
