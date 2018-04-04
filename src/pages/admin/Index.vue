df0d64825a4688a1e1ddc39b289ac702960b1ecb
iustin <iustin.nitza@gmail.com>
2018-04-04 14:08:53 +0200

    Fix layout

--- src/pages/admin/Index.vue
+++ src/pages/admin/Index.vue
@@ -2,13 +2,15 @@
   <div>
     <div class="admin-new">
       <div class="container">
-        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-          <ul class="admin-menu">
-            <li><router-link to="/admin">View Products</router-link></li>
-            <li><router-link to="/admin/new">New Product</router-link></li>
-          </ul>
+        <div class="row">
+          <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+            <ul class="admin-menu">
+              <li><router-link to="/admin">View Products</router-link></li>
+              <li><router-link to="/admin/new">New Product</router-link></li>
+            </ul>
+          </div>
+          <router-view></router-view>
         </div>
-        <router-view></router-view>
       </div>
     </div>
   </div>
