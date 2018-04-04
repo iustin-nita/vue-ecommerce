1672f07ee59eca0e617f3636fac235bf67f83036
Iustin Nita <iustin.nitza@gmail.com>
2018-04-04 23:09:35 +0200

    try to fix heroku

--- app.js
+++ app.js
@@ -58,9 +58,9 @@ app.use(express.static(distDir));
 app.use('/api/v1', api);
 
 // Initialize the app.
-var server = app.listen(process.env.PORT || 3001, function () {
-  var port = server.address().port;
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+// var server = app.listen(process.env.PORT || 3001, function () {
+//   var port = server.address().port;
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
 
 module.exports = app;
